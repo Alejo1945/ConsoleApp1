a87159056496b0d408069321fa1efed0e9ba3b09
Alejo1945 <ainavarretec@utn.edu.ec>
2025-07-01 12:38:23 -0500

    Agregar clases modelo

---
+++ Bodega.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Bodega
+    {
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public Bodega(string nombre, string direccion, string telefono, string email)
+        {
+            Nombre = nombre;
+            Direccion = direccion;
+            Telefono = telefono;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return $"Bodega: {Nombre}, Dirección: {Direccion}, Teléfono: {Telefono}, Email: {Email}";
+        }
+    }
+}
