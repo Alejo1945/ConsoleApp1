d5abf1fc65f309129f7b95b4a06aab37d0fdec65
Alejo1945 <ainavarretec@utn.edu.ec>
2025-07-01 12:40:26 -0500

    agregar funcionalidad de ventas en tiendas

---
+++ Tienda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Tienda
+    {
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public Tienda(string nombre, string direccion, string telefono)
+        {
+            Nombre = nombre;
+            Direccion = direccion;
+            Telefono = telefono;
+        }
+        public override string ToString()
+        {
+            return $"Tienda: {Nombre}, Dirección: {Direccion}, Teléfono: {Telefono}";
+        }
+    }
+}
